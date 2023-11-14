2ad0c2b6c250f4619661595e0645016c313ab2dd
NoobCoder <107041625+noobcoderwastaken@users.noreply.github.com>
2023-11-14 12:05:13 +0530

    Fixed one word issue

--- catsay.py
+++ catsay.py
@@ -5,7 +5,7 @@ from colorama import Fore
 colorama.init()
 
 try:
-    text = sys.argv[1]
+    text = " ".join(sys.argv[1:])  # Join all command line arguments to form the complete text
 
     print(Fore.GREEN + f"""
 
@@ -19,4 +19,4 @@ try:
     """)
 
 except:
-    print("Usage : python3 catsay.py <text>")
+    print("Usage: python3 catsay.py <text>")
