169da98349d8b6c334e5136551a4b122cdc55052
NoobCoder <107041625+noobcoderwastaken@users.noreply.github.com>
2023-11-15 10:59:51 +0530

    Added dialog box

--- catsay.py
+++ catsay.py
@@ -8,13 +8,13 @@ try:
     text = " ".join(sys.argv[1:])  # Join all command line arguments to form the complete text
 
     print(Fore.GREEN + f"""
-
-    _._     _,-'""`-._
-    (,-.`._,'(       |\`-/|
-        `-.-' \ )-`( , o o)
+                            ___________________
+                           <{text.capitalize()}>
+                            -------------------
+    _._     _,-'""`-._          /
+    (,-.`._,'(       |\`-/|   / 
+        `-.-' \ )-`( , o o) /
             `-    \`_`"'-
-
-    {text.capitalize()}
 
     """)
 
